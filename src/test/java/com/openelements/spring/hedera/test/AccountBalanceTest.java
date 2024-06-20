cc71a120c1fb614b8e4597d045c49ddd11f13ab8
Hendrik Ebbers <hendrik.ebbers@web.de>
2024-06-20 15:57:53 +0200

    Test refactored

--- src/test/java/com/openelements/spring/hedera/test/AccountBalanceTest.java
+++ src/test/java/com/openelements/spring/hedera/test/AccountBalanceTest.java
@@ -4,13 +4,17 @@ import com.hedera.hashgraph.sdk.AccountId;
 import com.hedera.hashgraph.sdk.Hbar;
 import com.openelements.spring.hedera.AccountBalanceRequest;
 import com.openelements.spring.hedera.AccountBalanceResult;
+import com.openelements.spring.hedera.EnableHedera;
 import com.openelements.spring.hedera.HederaClient;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@EnableHedera
 @SpringBootTest
+@SpringBootConfiguration
 public class AccountBalanceTest {
 
     @Autowired
@@ -19,8 +23,7 @@ public class AccountBalanceTest {
     @Test
     void testGetBalance() throws Exception {
         //given
-        AccountId accountId = AccountId.fromString("0.0.4457570");
-        AccountBalanceRequest accountBalanceRequest = new AccountBalanceRequest(accountId);
+        AccountBalanceRequest accountBalanceRequest = AccountBalanceRequest.of("0.0.4457570");
 
         //when
         final AccountBalanceResult accountBalanceResult = hederaClient.executeAccountBalanceQuery(
--- src/test/java/com/openelements/spring/hedera/test/TestConfig.java
+++
@@ -1,9 +0,0 @@
-package com.openelements.spring.hedera.test;
-
-import com.openelements.spring.hedera.EnableHedera;
-import org.springframework.boot.SpringBootConfiguration;
-
-@EnableHedera
-@SpringBootConfiguration
-public class TestConfig {
-}
