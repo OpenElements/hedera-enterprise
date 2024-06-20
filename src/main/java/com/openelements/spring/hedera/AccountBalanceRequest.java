735944ec93319d806688d7fd69c9990cb3d68c7b
Hendrik Ebbers <hendrik.ebbers@web.de>
2024-06-20 15:56:48 +0200

    null check

--- src/main/java/com/openelements/spring/hedera/AccountBalanceRequest.java
+++ src/main/java/com/openelements/spring/hedera/AccountBalanceRequest.java
@@ -3,4 +3,14 @@ package com.openelements.spring.hedera;
 import com.hedera.hashgraph.sdk.AccountId;
 
 public record AccountBalanceRequest(AccountId accountId) {
+
+    public AccountBalanceRequest {
+        if (accountId == null) {
+            throw new IllegalArgumentException("accountId must not be null");
+        }
+    }
+
+    public static AccountBalanceRequest of(String accountId) {
+        return new AccountBalanceRequest(AccountId.fromString(accountId));
+    }
 }
