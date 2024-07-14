54c72cab77e95aef32b6163c1f4d546d0f270694
Hendrik Ebbers <hendrik.ebbers@web.de>
2024-07-14 14:18:07 +0200

    verification of contract

--- spring-hedera/src/test/java/com/openelements/spring/hedera/test/ContractVerificationClientImplementationTest.java
+++ spring-hedera/src/test/java/com/openelements/spring/hedera/test/ContractVerificationClientImplementationTest.java
@@ -3,6 +3,7 @@ package com.openelements.spring.hedera.test;
 import com.hedera.hashgraph.sdk.ContractId;
 import com.openelements.hedera.base.HederaClient;
 import com.openelements.hedera.base.data.ContractVerificationState;
+import com.openelements.hedera.base.implementation.HederaNetwork;
 import com.openelements.spring.hedera.api.ContractVerificationClient;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -11,6 +12,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.condition.DisabledIf;
 
 @SpringBootTest(classes = TestConfig.class)
 class ContractVerificationClientImplementationTest {
@@ -20,13 +22,21 @@ class ContractVerificationClientImplementationTest {
     private HederaClient hederaClient;
 
     @Autowired
+    private HederaNetwork hederaNetwork;
+
+    @Autowired
     private ContractVerificationClient contractVerificationClient;
 
     private Path getResource(String resource) {
         return Path.of(ContractVerificationClientImplementationTest.class.getResource(resource).getPath());
     }
 
+    private boolean isNotSupportedChain() {
+        return hederaNetwork == HederaNetwork.CUSTOM;
+    }
+
     @Test
+    @DisabledIf("isNotSupportedChain")
     void test() throws Exception {
         //given
         final String contractName = "HelloWorld";
