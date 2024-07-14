9ffa3c6e57dbd52d9ffdd5fcd51f7baef166164f
Hendrik Ebbers <hendrik.ebbers@web.de>
2024-07-14 14:36:14 +0200

    solo

--- spring-hedera/src/main/java/com/openelements/spring/hedera/implementation/HederaAutoConfiguration.java
+++ spring-hedera/src/main/java/com/openelements/spring/hedera/implementation/HederaAutoConfiguration.java
@@ -25,7 +25,7 @@ public class HederaAutoConfiguration {
     private final static Logger log = LoggerFactory.getLogger(HederaAutoConfiguration.class);
 
     @Bean
-    public HederaNetwork hederaNetwork(final HederaProperties properties) {
+    HederaNetwork hederaNetwork(final HederaProperties properties) {
         if(properties.getNetwork() == null) {
             throw new IllegalArgumentException("'spring.hedera.network' property must be set");
         }
@@ -41,7 +41,7 @@ public class HederaAutoConfiguration {
     }
 
     @Bean
-    public PrivateKey privateKey(final HederaProperties properties) {
+    PrivateKey privateKey(final HederaProperties properties) {
         final String privateKey = properties.getPrivateKey();
         if(privateKey == null) {
             throw new IllegalArgumentException("'spring.hedera.privateKey' property must be set");
@@ -50,14 +50,14 @@ public class HederaAutoConfiguration {
             throw new IllegalArgumentException("'spring.hedera.privateKey' property must not be blank");
         }
         try {
-            return PrivateKey.fromString(properties.getPrivateKey());
+            return PrivateKey.fromString(privateKey);
         } catch (Exception e) {
             throw new IllegalArgumentException("Can not parse 'spring.hedera.privateKey' property", e);
         }
     }
 
     @Bean
-    public AccountId accountId(final HederaProperties properties) {
+    AccountId accountId(final HederaProperties properties) {
         final String accountId = properties.getAccountId();
         if(accountId == null) {
             throw new IllegalArgumentException("'spring.hedera.accountId' property must be set");
@@ -66,14 +66,14 @@ public class HederaAutoConfiguration {
             throw new IllegalArgumentException("'spring.hedera.accountId' property must not be blank");
         }
         try {
-            return AccountId.fromString(properties.getAccountId());
+            return AccountId.fromString(accountId);
         } catch (Exception e) {
             throw new IllegalArgumentException("Can not parse 'spring.hedera.accountId' property", e);
         }
     }
 
     @Bean
-    public Client client(final HederaProperties properties, AccountId accountId, PrivateKey privateKey, HederaNetwork hederaNetwork) {
+    Client client(final HederaProperties properties, AccountId accountId, PrivateKey privateKey, HederaNetwork hederaNetwork) {
         try {
             final Client client;
             if(hederaNetwork != HederaNetwork.CUSTOM) {
@@ -108,12 +108,12 @@ public class HederaAutoConfiguration {
     }
 
     @Bean
-    public HederaClient hederaClient(final Client client) {
+    HederaClient hederaClient(final Client client) {
         return new HederaClientImpl(client);
     }
 
     @Bean
-    public ContractVerificationClient contractVerificationClient(HederaNetwork hederaNetwork) {
+    ContractVerificationClient contractVerificationClient(HederaNetwork hederaNetwork) {
         if(hederaNetwork == HederaNetwork.CUSTOM) {
             throw new IllegalArgumentException("Contract verification is not supported for custom networks");
         }
